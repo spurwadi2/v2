c6d6f07cd83d0023f7b483ebc4e0e50a5a32f2d3
Sigit Purwadi <sgt2426@gmail.com>
2020-02-11 00:55:49 +0700

    Add gulp imagemin compression

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { series, parallel, src, dest, watch, gulp } = require('gulp');
+const { series, parallel, src, dest, watch, gulp, path } = require('gulp');
 
 // gulp plugins and utils
 const sourcemaps = require('gulp-sourcemaps');
@@ -7,6 +7,8 @@ const gutil = require('gulp-util');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const livereload = require('gulp-livereload');
+const del = require('del'); // delete .temp folder after imagemin
+const extReplace = require("gulp-ext-replace"); // webp convert
 
 // postcss plugins
 const autoprefixer = require('autoprefixer');
@@ -15,11 +17,89 @@ const easyimport = require('postcss-easy-import');
 const customProperties = require('postcss-custom-properties');
 const colorFunction = require('postcss-color-function');
 
+// imagemin compressions
+const imagemin = require('gulp-imagemin');
+const imageminGuetzli = require('imagemin-guetzli');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminGiflossy = require('imagemin-giflossy');
+const imageminWebp = require('imagemin-webp');
+//const imageminOptipng = require('imagemin-optipng');
+//const imageminSvgo = require('imagemin-svgo');
+
+
+const paths = {
+    styles: {
+        src: 'assets/css/*.css',
+        dest: 'assets/built/'
+    },
+    scripts: {
+        src: 'assets/js/*.js',
+        dest: 'assets/built/'
+    },
+    images: {
+        src: 'assets/uncompress/*',
+        dest: 'assets/.temp/'
+    }
+}
+
+// ---------------------------------------------------------- //
+
+// .webp converter
+function webp() {
+    return src('assets/uncompress/*')
+        .pipe(imagemin( [
+            imageminWebp( {
+                quality: 50
+            })
+        ]))
+        .pipe(extReplace('.webp'))
+        .pipe(dest('assets/.temp/'))
+};
+
+// image compression task
+function giflossyTask() {
+    return src('assets/uncompress/*.gif')
+        .pipe(imagemin( [
+            imageminGiflossy( {
+                lossy: 80
+            })
+        ]))
+        .pipe(dest('assets/.temp/'))
+};
+
+function cleanTemp() {
+    return del(['assets/.temp/*.jpg', 'assets/uncompress/*.jpg']);
+};
+
+function mozjpegTask() {
+    return src('assets/.temp/*.jpg')
+        .pipe(imagemin( [
+            imageminMozjpeg( {
+                quality: 85,
+                progressive: true
+            })
+        ]))
+        .pipe(dest('assets/images/'))
+        .pipe(livereload())
+};
+
+function guetzliTask() {
+    return src('assets/uncompress/*.jpg')
+        .pipe(imagemin( [
+            imageminGuetzli( {
+                quality: 85
+            })
+        ]))
+        .pipe(dest('assets/.temp/'));
+};
+
+// end of update (don't change script bellow)
 
 function watchTask() {
     livereload.listen();
     watch('assets/css/*.css', (cssTask));
     watch('assets/js/*.js', (jsTask));
+    //watch('assets/uncompress/*.jpg', series(guetzliTask, mozjpegTask, cleanTemp, watchTask));
 };
 
 function jsTask() {
@@ -51,4 +131,9 @@ function swallowError(error) {
     gutil.beep()
     this.emit('end')
 };
+
+
+// gulp main task list
+exports.webp = series(webp); // convert all image to webp
+exports.imagemin = series(parallel(giflossyTask, guetzliTask), mozjpegTask); // compress images
 exports.default = series(parallel(cssTask, jsTask), watchTask);
